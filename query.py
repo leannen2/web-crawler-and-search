c05d3895f0621ea9dc0c4f27110ad48209cd5584
Wilhelm Wang <wilhelaw@uci.edu>
2023-03-05 19:22:34 -0800

    start on calculating Cosine score

--- query.py
+++ query.py
@@ -13,3 +13,34 @@ for i in range(0,20):
     linksList.append(bookkeepingDict[docId])
 
 print(linksList, numberLinks)
+
+
+
+# calculate the score of a document given the query
+def queryScore(query, document):
+    # we get the sum of the query/document weight
+    res = 0
+    for word in query:
+        # obtain the weight of the word in the document
+        index = json.load(open("index.json"))
+        for token in index:
+            if token == word:
+                for doc in index[token]:
+                    if doc == document:
+                        res += doc['tfidf']
+                break   # saves time? (because there's no need to iterate through rest of tokens if we already found)
+            
+    return res
+
+
+
+# def main():
+#     query = ""
+
+#     DocScores = {}  # dict to track document's query scores
+#     # for every document, calculate the query score
+#     for doc in document_list:
+#         DocScores[doc] = queryScore(query, doc)
+
+#     # now rank the documents best to last (the search results)
+#     return sorted(DocScores)
