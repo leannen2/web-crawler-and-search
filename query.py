2eed7890e13c9b24350ea68defdd1249f961b6cc
Wilhelm Wang <wilhelaw@uci.edu>
2023-03-08 16:00:57 -0800

    updated query, now returns list of docids

--- query.py
+++ query.py
@@ -19,53 +19,55 @@ print(linksList, numberLinks)
 
 
 # calculate the score of a document given the query word
-def queryScore(document, querydict, querylength):
+def queryScore(query):
+
+    # this keeps track of how many times each word appears in the query
+    queryCount = defaultdict(int)
+    for word in query.lower.split():
+        queryCount[word] += 1
+
+
+    # quickly compute the query weights
+    # and query length once (normalization denominator)
+    docFreqindex = json.load(open("docFreq.json"))
+    queryWeight = {}
+    sum = 0
+    for word in queryCount:
+        for token in docFreqindex:
+            if token == word:
+                weight = ( 1 + math.log10(queryCount[word]) ) * docFreqindex[word]
+                queryWeight[word] = weight
+                sum +=  weight**2
+    querylength = math.sqrt(sum)
+    
+
+
+    DOCSCORES = {}
 
     index = json.load(open("indexWithNormalizedWeights.json"))
-    docFreqindex = json.load(open("docFreq.json"))
     
-    res = 0
     # get the query score for every word in the query
-    for word in querydict:
+    for word in queryCount:
         # iterate through all words and find the tokens that match the query word
-        for token in index:
-            if token == word:
-                for doc in index[token]:
-                    if doc == document:
-                        prenormalized = ( 1 + math.log10(querydict[word]) ) * docFreqindex[word]
-                        res += (prenormalized)/querylength * doc['weight']
+            for doc in index[word]:    
+                DOCSCORES[doc["docId"]] += (queryWeight[word])/querylength * doc["weight"]
 
-                    break      
-                break   # saves time? (because there's no need to iterate through rest of tokens if we already found)
 
-    return res
+    # return the top 20 results
+    sortedScores = list( sorted(DOCSCORES.items(), key=lambda x:x[1]).keys() )
+    results = []
+    i = 0
+    while i < 20:
+        results.append(sortedScores[i])
+        i += 1
+
+    return results
+
+
 
 
 
 def main():
     query = "best car insurance"
 
-    # this keeps track of how many times each word appears in the query
-    querydict = defaultdict(int)
-    for word in query.lower.split():
-        querydict[word] += 1
-
-    # quickly compute the query length once, to get the normalization denominator to pass in function
-    sum = 0
-    docFreqindex = json.load(open("docFreq.json"))
-    for word in querydict:
-        for token in docFreqindex:
-            if token == word:
-                sum +=  ( ( 1 + math.log10(querydict[word]) ) * docFreqindex[word] )**2
-    querylength = math.sqrt(sum)
-    
-    DocScores = {}  # dict to track document's query scores
-    # for every document, calculate the query score
-    
-    for doc in document_list:
-        DocScores[doc] = queryScore(doc, querydict, querylength)
-
-
-
-    # now rank the documents best to last (the search results)
-    return sorted(DocScores)[0]
+    queryScore(query)
