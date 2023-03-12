f6d3c6f8c6c5b5b1d2e2c81bcc20c48e15184534
Wilhelm Wang <wilhelaw@uci.edu>
2023-03-11 22:01:29 -0800

    fixed bugs, no html tagging yet

--- query.py
+++ query.py
@@ -2,19 +2,19 @@ import json
 import math
 from collections import defaultdict
 
-query = input("Please enter user input: ")
-with open('index.json') as json_file:
-    indexDict = json.load(json_file)
-with open('bookkeeping.json') as json_file:
-    bookkeepingDict = json.load(json_file)
-links = indexDict[query]
-linksList = list()
-numberLinks = len(links)
-for i in range(0,20):
-    docId = (list(links[i].values())[0])
-    linksList.append(bookkeepingDict[docId])
+# query = input("Please enter user input: ")
+# with open('index.json') as json_file:
+#     indexDict = json.load(json_file)
+# with open('bookkeeping.json') as json_file:
+#     bookkeepingDict = json.load(json_file)
+# links = indexDict[query]
+# linksList = list()
+# numberLinks = len(links)
+# for i in range(0,20):
+#     docId = (list(links[i].values())[0])
+#     linksList.append(bookkeepingDict[docId])
 
-print(linksList, numberLinks)
+# print(linksList, numberLinks)
 
 
 
@@ -23,7 +23,8 @@ def queryScore(query):
 
     # this keeps track of how many times each word appears in the query
     queryCount = defaultdict(int)
-    for word in query.lower.split():
+    wordlst = query.lower().split()
+    for word in wordlst:
         queryCount[word] += 1
 
 
@@ -42,28 +43,27 @@ def queryScore(query):
     
 
 
-    DOCSCORES = {}
+    DOCSCORES = defaultdict(int)
 
     index = json.load(open("indexWithNormalizedWeights.json"))
     
     # get the query score for every word in the query
     for word in queryCount:
         # iterate through all words and find the tokens that match the query word
-            for doc in index[word]:    
-                DOCSCORES[doc["docId"]] += (queryWeight[word])/querylength * doc["weight"]
+        for doc in index[word]:    
+            DOCSCORES[doc["docId"]] += (queryWeight[word])/querylength * doc["weight"]
 
 
     # return the top 20 results
-    sortedScores = list( sorted(DOCSCORES.items(), key=lambda x:x[1]).keys() )
+    sortedScores = sorted(DOCSCORES.items(), key=lambda x:x[1], reverse=True)
     results = []
     i = 0
     while i < 20:
         results.append(sortedScores[i])
         i += 1
 
+    print(results)
     return results
-
-
 
 
 
@@ -71,3 +71,7 @@ def main():
     query = "best car insurance"
 
     queryScore(query)
+
+
+
+main()
