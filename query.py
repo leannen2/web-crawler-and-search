f5e4cf828b3070ced4eb09e6677f3dee2b057c1b
Leanne <leannen2@uci.edu>
2023-03-12 05:09:05 -0700

    added lemmatizing for query terms

--- query.py
+++ query.py
@@ -1,6 +1,7 @@
 import json
 import math
 from collections import defaultdict
+from nltk.stem import WordNetLemmatizer
 
 # query = input("Please enter user input: ")
 # with open('index.json') as json_file:
@@ -16,16 +17,38 @@ from collections import defaultdict
 
 # print(linksList, numberLinks)
 
+def tokenize(text):
+    stopWords = ['a', 'about', 'above', 'after', 'again', 'against', 'all', 'am', 'an', 'and', 'any', 'are', "aren't", 'as', 'at', 'be', 'because', 'been', 'before', 'being', 'below', 'between', 'both', 'but', 'by', "can't", 'cannot', 'could', "couldn't", 'did', "didn't", 'do', 'does', "doesn't", 'doing', "don't", 'down', 'during', 'each', 'few', 'for', 'from', 'further', 'had', "hadn't", 'has', "hasn't", 'have', "haven't", 'having', 'he', "he'd", "he'll", "he's", 'her', 'here', "here's", 'hers', 'herself', 'him', 'himself', 'his', 'how', "how's", 'i', "i'd", "i'll", "i'm", "i've", 'if', 'in', 'into', 'is', "isn't", 'it', "it's", 'its', 'itself', "let's", 'me', 'more', 'most', "mustn't", 'my', 'myself', 'no', 'nor', 'not', 'of', 'off', 'on', 'once', 'only', 'or', 'other', 'ought', 'our', 'ours\tourselves', 'out', 'over', 'own', 'same', "shan't", 'she', "she'd", "she'll", "she's", 'should', "shouldn't", 'so', 'some', 'such', 'than', 'that', "that's", 'the', 'their', 'theirs', 'them', 'themselves', 'then', 'there', "there's", 'these', 'they', "they'd", "they'll", "they're", "they've", 'this', 'those', 'through', 'to', 'too', 'under', 'until', 'up', 'very', 'was', "wasn't", 'we', "we'd", "we'll", "we're", "we've", 'were', "weren't", 'what', "what's", 'when', "when's", 'where', "where's", 'which', 'while', 'who', "who's", 'whom', 'why', "why's", 'with', "won't", 'would', "wouldn't", 'you', "you'd", "you'll", "you're", "you've", 'your', 'yours', 'yourself', 'yourselves']
+    tokens = ''
+    for a in text:
+        if a.isalnum() ==  False or a.isascii() == False:
+            tokens+= ' '
+        else:
+            tokens+=a
+    tokens = tokens.lower().split()
+    lemmatize_tokens(tokens)
+    tokens = list(filter(lambda x: x not in stopWords and len(x) > 2, tokens))
+    token_count = {}
+    for token in tokens:
+        if token not in token_count:
+            token_count[token] = 1
+        else:
+            token_count[token] += 1
 
+    return token_count
+
+def lemmatize_tokens(tokens):
+    lemmatizer = WordNetLemmatizer()
+    for index in range(len(tokens)):
+        for l in ['n','v', 'a', 'r', 's']:
+            lemma = lemmatizer.lemmatize(tokens[index], pos=l)
+            if lemma != tokens[index]:
+                tokens[index] = lemma
+                break
 
 # calculate the score of a document given the query word
 def queryScore(query):
-
-    # this keeps track of how many times each word appears in the query
-    queryCount = defaultdict(int)
-    wordlst = query.lower().split()
-    for word in wordlst:
-        queryCount[word] += 1
+    queryCount = tokenize(query)
 
 
     # quickly compute the query weights
@@ -73,7 +96,7 @@ def queryScore(query):
 
 
 def main():
-    query = "Irvine"
+    query = "artificial intelligence"
 
     queryScore(query)
 
